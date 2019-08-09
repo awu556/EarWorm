97734a2f1c5d7e1b830d49d45d555abc7c1c1217
Wutang445 <austinw556@gmail.com>
2019-08-08 21:51:01 -0400

    setting up Musix search API

--- server/api/musixAPI.js
+++ server/api/musixAPI.js
@@ -1,25 +1,10 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
 const music = require('musicmatch')({
   apikey: '19c8312e5f65e34e70e7361977223254'
 })
 module.exports = router
 
 router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
-
-router.get('/music', async (req, res, next) => {
   try {
     const data = await music.trackSearch({
       q_lyrics: 'karma police arrest this man',
